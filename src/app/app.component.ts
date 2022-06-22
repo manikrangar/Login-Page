d25cce41d99215fb9beda823b6979c2d15df134e
manikrangar <manikrangarx@gmail.com>
2022-06-22 19:07:43 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,36 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  actName="Elon Musk";
+  actPassword="123456789"
+  nameNotFound="";
+  passwordNotFound="";
   title = 'helloangular';
+  name:String='';
+  password:String='';
+  
+  setValue(name:String , password:String){
+    console.log(name,password)
+    this.name=name;
+    this.password=password;
+    
+
+    if(this.name==="")
+    this.nameNotFound="Enter Your Name";
+    
+    if(this.password==="")
+    this.passwordNotFound="Enter Your Password"
+
+    if(this.actName==this.name && this.actPassword ==this.password)
+    alert("Logged In Success")
+    else
+    if(!(this.name==""||this.password==""))
+    alert("Sorry Invalid Credentials");
+    window.location.reload();
+  }
+  check()
+  {
+    console.log(this.name,this.password);
+    alert("Name = " + this.name + "   \nPassword =" +  this.password); 
+  }
 }
